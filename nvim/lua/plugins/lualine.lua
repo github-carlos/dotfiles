d66d99ae5beac57120970c1bdcc88f6e71fbf81c
carlos.bueno@asksuite.com <carlos.bueno@asksuite.com>
2024-04-16 09:53:18 -0300

    saving changes

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,16 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "catppuccin",
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "catppuccin",
+      },
+      sections = {
+        lualine_a = { {
+          'filename',
+          path = 1
+        } }
+      }
+    })
+  end,
 }
